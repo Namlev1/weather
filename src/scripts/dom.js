24977a0fadb14e8ad1ab51470b18849d8e8ffd90
namlev <mikipatkowski@protonmail.com>
2024-07-25 13:25:02 +0200

    Style city input

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,9 +1,10 @@
 const main = document.querySelector('#main')
+import Magnify from '../assets/icons/magnify.svg'
 
 function createDailyForecastDiv() {
   const forecastContainer = document.createElement('div')
   forecastContainer.id = 'daily-forecast'
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i += 1) {
     const forecastDiv = document.createElement('div')
     forecastDiv.classList.add('card')
     const day = document.createElement('p')
@@ -38,12 +39,36 @@ function createMainForecastDiv() {
   return div
 }
 
+function createInputWrap() {
+  const inputWrap = document.createElement('div')
+  inputWrap.id = 'input-wrap'
+
+  const cityInput = document.createElement('input')
+  cityInput.type = 'text'
+
+  const searchBtn = document.createElement('img')
+  searchBtn.classList.add('search-btn')
+  searchBtn.src = Magnify
+  searchBtn.alt = 'Search button'
+  searchBtn.addEventListener('click', () => {
+    console.log(cityInput.value)
+  })
+
+  inputWrap.appendChild(cityInput)
+  inputWrap.appendChild(searchBtn)
+  return inputWrap
+}
+
 export function createDomStructure() {
   const leftDiv = document.createElement('div')
   leftDiv.id = 'left'
   const rightDiv = document.createElement('div')
   rightDiv.id = 'right'
+  const inputWrap = createInputWrap()
+
   const mainForecastDiv = createMainForecastDiv()
+
+  leftDiv.appendChild(inputWrap)
   leftDiv.appendChild(mainForecastDiv)
   main.appendChild(leftDiv)
   main.appendChild(rightDiv)
