df7a7c086257f58586f0a99f76e45014996541e2
namlev <mikipatkowski@protonmail.com>
2024-07-27 10:59:06 +0200

    Finish right panel DOM

--- src/scripts/domStructure.js
+++ src/scripts/domStructure.js
@@ -1,33 +1,105 @@
+import { format } from 'date-fns'
 import Magnify from '../assets/icons/magnify.svg'
 import Wind from '../assets/icons/wind.svg'
 import Drop from '../assets/icons/drop.svg'
-import { format } from 'date-fns'
 
 const main = document.querySelector('#main')
+
+function createCard() {
+  const forecastDiv = document.createElement('div')
+  forecastDiv.classList.add('card')
+
+  const day = document.createElement('p')
+  day.textContent = 'Wed'
+  const temp = document.createElement('p')
+  temp.textContent = '20°C'
+  const img = document.createElement('img')
+  img.classList.add('img-shadow')
+  img.src = 'https://openweathermap.org/img/wn/10d@2x.png'
+
+  forecastDiv.appendChild(day)
+  forecastDiv.appendChild(temp)
+  forecastDiv.appendChild(img)
+  return forecastDiv
+}
+
+function createInputDiv() {
+  const inputWrap = document.createElement('div')
+  inputWrap.id = 'input-wrap'
+
+  const cityInput = document.createElement('input')
+  cityInput.type = 'text'
+  cityInput.value = 'Warsaw'
+
+  const searchBtn = document.createElement('img')
+  searchBtn.classList.add('search-btn')
+  searchBtn.src = Magnify
+  searchBtn.alt = 'Search button'
+  searchBtn.addEventListener('click', () => {
+    console.log(cityInput.value)
+  })
+
+  inputWrap.appendChild(cityInput)
+  inputWrap.appendChild(searchBtn)
+  return inputWrap
+}
 
 function createDailyForecastDiv() {
   const forecastContainer = document.createElement('div')
   forecastContainer.id = 'daily-forecast'
 
   for (let i = 0; i < 5; i += 1) {
-    const forecastDiv = document.createElement('div')
-    forecastDiv.classList.add('card')
-
-    const day = document.createElement('p')
-    day.textContent = 'Wed'
-    const temp = document.createElement('p')
-    temp.textContent = '20°C'
-    const img = document.createElement('img')
-    img.classList.add('img-shadow')
-    img.src = 'https://openweathermap.org/img/wn/10d@2x.png'
-
-    forecastDiv.appendChild(day)
-    forecastDiv.appendChild(temp)
-    forecastDiv.appendChild(img)
-
+    const forecastDiv = createCard()
     forecastContainer.appendChild(forecastDiv)
   }
   return forecastContainer
+}
+
+function createTodayRightDiv() {
+  const todayDiv = document.createElement('div')
+  todayDiv.id = 'today-right'
+  const icon = document.createElement('img')
+  icon.classList.add('img-shadow')
+  icon.alt = 'Weather icon'
+  icon.src = 'https://openweathermap.org/img/wn/10d@2x.png'
+  const minP = document.createElement('p')
+  const minTemp = document.createElement('p')
+  const maxP = document.createElement('p')
+  const maxTemp = document.createElement('p')
+
+  minP.textContent = 'min:'
+  minTemp.textContent = '20.89°C'
+  maxP.textContent = 'max:'
+  maxTemp.textContent = '19.80°C'
+
+  const city = document.createElement('p')
+  city.id = 'city-right'
+  city.textContent = 'Warsaw'
+
+  todayDiv.appendChild(icon)
+  todayDiv.appendChild(minP)
+  todayDiv.appendChild(minTemp)
+  todayDiv.appendChild(maxP)
+  todayDiv.appendChild(maxTemp)
+  todayDiv.appendChild(city)
+
+  return todayDiv
+}
+
+function createHourlyForecastDiv() {
+  const div = document.createElement('div')
+  div.id = 'hourly-forecast'
+
+  const header = document.createElement('p')
+  header.textContent = 'Hourly Forecast'
+  div.appendChild(header)
+
+  for (let i = 0; i < 6; i += 1) {
+    const card = createCard()
+    div.appendChild(card)
+  }
+
+  return div
 }
 
 function createMainForecastDiv() {
@@ -79,34 +151,6 @@ function createMainForecastDiv() {
   return div
 }
 
-function createInputDiv() {
-  const inputWrap = document.createElement('div')
-  inputWrap.id = 'input-wrap'
-
-  const cityInput = document.createElement('input')
-  cityInput.type = 'text'
-
-  const searchBtn = document.createElement('img')
-  searchBtn.classList.add('search-btn')
-  searchBtn.src = Magnify
-  searchBtn.alt = 'Search button'
-  searchBtn.addEventListener('click', () => {
-    console.log(cityInput.value)
-  })
-
-  inputWrap.appendChild(cityInput)
-  inputWrap.appendChild(searchBtn)
-  return inputWrap
-}
-
-function formatDate(date) {
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0') // getMonth() is zero-based
-  const year = date.getFullYear()
-
-  return `${day}.${month}.${year}`
-}
-
 function createLeftDiv() {
   const leftDiv = document.createElement('div')
   leftDiv.id = 'left'
@@ -118,55 +162,26 @@ function createLeftDiv() {
   return leftDiv
 }
 
-function createTodayRightDiv() {
-  const todayDiv = document.createElement('div')
-  todayDiv.id = 'today-right'
-  const icon = document.createElement('img')
-  icon.classList.add('img-shadow')
-  icon.alt = 'Weather icon'
-  icon.src = 'https://openweathermap.org/img/wn/10d@2x.png'
-  const minP = document.createElement('p')
-  const minTemp = document.createElement('p')
-  const maxP = document.createElement('p')
-  const maxTemp = document.createElement('p')
-
-  minP.textContent = 'min:'
-  minTemp.textContent = '20.89°C'
-  maxP.textContent = 'max:'
-  maxTemp.textContent = '19.80°C'
-
-  const city = document.createElement('p')
-  city.id = 'city-right'
-  city.textContent = 'Warsaw'
-
-  todayDiv.appendChild(icon)
-  todayDiv.appendChild(minP)
-  todayDiv.appendChild(minTemp)
-  todayDiv.appendChild(maxP)
-  todayDiv.appendChild(maxTemp)
-  todayDiv.appendChild(city)
-
-  return todayDiv
-}
-
 function createRightDiv() {
   const rightDiv = document.createElement('div')
   rightDiv.id = 'right'
 
   const today = new Date()
   const date = document.createElement('p')
-  date.classList.add('date')
+  date.classList.add('header-text')
   const time = document.createElement('p')
-  time.classList.add('time')
+  time.classList.add('header-text')
 
   date.textContent = format(today, 'dd.MM.yyyy')
   time.textContent = format(today, 'HH:mm a')
 
   const todayDiv = createTodayRightDiv()
+  const hourlyForecast = createHourlyForecastDiv()
 
   rightDiv.appendChild(date)
   rightDiv.appendChild(time)
   rightDiv.appendChild(todayDiv)
+  rightDiv.appendChild(hourlyForecast)
   return rightDiv
 }
 
